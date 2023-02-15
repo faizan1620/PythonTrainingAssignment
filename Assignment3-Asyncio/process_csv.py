0344b5894de2abe4c6f82814ee62efdb7d6534f0
Faizan  Alam <faizan.alam@sourcefuse.com>
2023-02-15 22:00:43 +0530

    fix: changes made as per PR comments

--- Assignment3-Asyncio/process_csv.py
+++ Assignment3-Asyncio/process_csv.py
@@ -3,20 +3,26 @@ import csv
 import asyncio
 
 async def display_line(reader_obj, writer):
+    writer.writerow(['Passenger with id greater than 1000 who survived'])
+    writer.writerow(['  - - - - - - - - - - - - - - - - - - - - - -  '])  # Seperator
     for row in reader_obj:
         print(row)
         await write_result(row, writer)
 
 
-# This function taking the id and keeping only that id which is greater than 1000 and savong to a results.csv file
+# This function taking the id and keeping only that id which is greater than 1000 which survived, to a results.csv file
 async def write_result(result, writer):
     async with asyncio.Lock():   # lock for gracefully write to shared file object
-        res = [id for id in result if id != 'PassengerId' and id != 'Survived' and int(id) > 1000] 
-        writer.writerow(res)
+        # id = -1 # Initialise with some invalid id
+        # if result[0] != 'PassengerId':
+        id = int(result[0])
+        if id > 1000 and int(result[1]):
+            writer.writerow([id])
 
 async def process_csv(path):
-    with open(path) as input_file, open('results.csv', 'a') as csv_out:
+    with open(path) as input_file, open('results.csv', 'w') as csv_out:
         reader_obj = csv.reader(input_file)
+        next(reader_obj)
         writer = csv.writer(csv_out, delimiter=',')
       
         await display_line(reader_obj, writer)
