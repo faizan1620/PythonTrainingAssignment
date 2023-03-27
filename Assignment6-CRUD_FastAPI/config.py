103733779507ad18cc276104c80d5636e2ae8dea
Faizan  Alam <faizan.alam@sourcefuse.com>
2023-03-27 22:38:12 +0530

    fix: Modified Config class

--- Assignment6-CRUD_FastAPI/config.py
+++ Assignment6-CRUD_FastAPI/config.py
@@ -1,23 +1,17 @@
 from dotenv import load_dotenv
 import os
-from pydantic import BaseModel
-
 load_dotenv()
+class Config():
+    def __init__(self, db_user:str, db_password:str, db_server:str, db_name:str):
+        self.db_user = db_user
+        self.db_password = db_password
+        self.db_server = db_server
+        self.db_name = db_name
 
 
-class Config(BaseModel):
-    db_user: str
-    db_password: str
-    db_server: str
-    db_name: str
+db_user = os.environ.get('DB_USER','')
+db_password =  os.environ.get('DB_PASSWORD','')
+db_server = os.environ.get('DB_SERVER','localhost')
+db_name = os.environ.get('DB_Name','student')
 
-
-Config.db_user = os.environ.get('DB_USER')
-
-Config.db_password =  os.environ.get('DB_PASSWORD')
-
-Config.db_server = os.environ.get('DB_SERVER')
-
-Config.db_name = os.environ.get('DB_Name')
-
-print(Config, Config.db_user)
+config = Config(db_user, db_password, db_server, db_name)
--- Assignment6-CRUD_FastAPI/services/database.py
+++ Assignment6-CRUD_FastAPI/services/database.py
@@ -1,13 +1,10 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from config import Config
+from config import config
 
 #SQLALCHEMY_DATABASE_URL = "postgresql://localhost:5432/student"
-
-SQLALCHEMY_DATABASE_URL = f"postgresql://{Config.db_user}:{Config.db_password}@{Config.db_server}/{Config.db_name}"
-
-print(SQLALCHEMY_DATABASE_URL)
+SQLALCHEMY_DATABASE_URL = f"postgresql://{config.db_user}:{config.db_password}@{config.db_server}/{config.db_name}"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL
